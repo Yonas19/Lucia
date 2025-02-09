342cda5e1b278dd00415ba99b4652a6e348f44fa
Yonas19 <yonjr0936@gmail.com>
2025-02-09 23:15:14 +0400

    animating the hair type section

--- Lucia/src/Paywall.jsx
+++ Lucia/src/Paywall.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import "./index.css";
+import a from "././Images/4c.jpg";
+import b from "././Images/1c.jfif";
+import c from "././Images/2a.jfif";
+import d from "././Images/3b.jfif";
+import e from "././Images/wavy.webp";
+import f from "././Images/2c.jfif";
 
 const Paywall = () => {
   const navigate = useNavigate();
@@ -11,35 +18,26 @@ const Paywall = () => {
   };
 
   return (
-    <div style={styles.container}>
-      <h1 style={styles.text}>Unlock Lucia</h1>
-      <button style={styles.button} onClick={handlePayment}>
-        
-      </button>
+    <div className="paywall-bg">
+      <h1>Lucia</h1>
+      <h4>በፀጉሮ ወይም በቆዳዎ ላይ ለውጥ ማየት አቅቶል ???</h4>
+      <h3>Hair type</h3>
+      <div className="marquee">
+        <div className="marquee-content">
+          <img src={a} alt="" />
+          <img src={b} alt="" />
+          <img src={c} alt="" />
+          <img src={d} alt="" />
+          <img src={e} alt="" />
+          <img src={f} />
+        </div>
+      </div>
+
+      <h3>Skin types</h3>
+
+      <button onClick={handlePayment}>Choose for your skin or hair</button>
     </div>
   );
-};
-
-const styles = {
-  container: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-    height: "100vh",
-    backgroundColor: "#fff",
-  },
-  text: {
-    fontSize: "1.5rem",
-    marginBottom: "20px",
-  },
-  button: {
-    padding: "10px 20px",
-    fontSize: "1rem",
-    backgroundColor: "#ffde59",
-    border: "none",
-    cursor: "pointer",
-  },
 };
 
 export default Paywall;
