0ec02a834358f487bd525beebbcfb6c3acabb1bf
Yonas19 <yonjr0936@gmail.com>
2025-02-11 23:53:43 +0400

    payment page initialized

---
+++ Lucia/src/forsreak.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function forsreak() {
+    return (
+      <>
+    <div>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi dolorem fugiat, culpa eum facilis quo id ad officiis excepturi voluptas explicabo non magni, nemo ipsam amet laboriosam accusamus sapiente? Ab libero ut dolores aspernatur obcaecati. Alias eveniet veniam perspiciatis ea dolorem temporibus nisi, laborum blanditiis beatae sit esse fuga enim quasi, exercitationem quo consectetur cum ipsa. Est reiciendis nisi, cupiditate illo cum architecto magni numquam impedit praesentium labore reprehenderit iste voluptatum id alias delectus ab dolorem non. Labore maxime perferendis, odit delectus eligendi alias? Quod soluta delectus, dignissimos qui ipsum magni nostrum error mollitia fuga! Temporibus repudiandae quis eum veritatis debitis architecto iste voluptatibus officia reprehenderit nobis, explicabo aperiam voluptate modi nesciunt culpa hic delectus at itaque, eaque perferendis ab consequatur beatae? Libero sunt nisi temporibus, veniam optio omnis? Cupiditate, corporis non vitae fugit accusantium earum. Enim, sunt a? Beatae, iste amet? Quasi assumenda, labore eaque alias magnam perferendis laudantium modi quisquam cumque, saepe qui molestias fugiat expedita aliquid nemo est culpa, sit mollitia dolorum soluta corrupti iure tempora. Impedit accusamus eos soluta eaque omnis, suscipit rerum numquam commodi veniam at laborum vitae possimus fugit sed sapiente rem error consequatur. Aliquam atque aspernatur quos odio in, molestias commodi, impedit sed quam animi consectetur voluptatem. Atque fugit molestias sapiente! Fugit ipsum repudiandae quia, omnis aspernatur iusto beatae, voluptas harum ad nobis doloremque delectus velit earum dolor tempora pariatur aperiam cupiditate corrupti repellat consectetur minus aliquam aliquid. Fugiat reiciendis quibusdam cumque ut nobis distinctio perferendis numquam, molestias laboriosam eveniet, facere a accusamus obcaecati fugit provident laborum. Commodi pariatur impedit beatae deleniti fuga placeat excepturi tempore eos similique nulla recusandae molestiae exercitationem minima labore quisquam corrupti unde, doloremque nisi consectetur illum voluptas? Voluptates ipsa, nihil optio perspiciatis, iusto voluptatem ab facilis asperiores aliquid commodi distinctio? Ullam nemo voluptatem, aliquam incidunt ad expedita praesentium!
+      </div>
+      <div className="monkey">
+              
+      </div>
+      </ >
+  )
+}
+
+export default forsreak
